809e6c0adfe5665ac3340ae3a65514d51ee4e533
Sam <113455539+SamShrubo@users.noreply.github.com>
2024-02-19 21:01:08 -0600

    parsed veer eh1

--- ds_parser.py
+++ ds_parser.py
@@ -7,6 +7,7 @@ df_code = ps.read_csv("dataframe.csv")
 
 with open('code_segments.json','a') as f:
     for row in df_code['text'].values:
+        print("Adding row...")
         dic={"text":str(row)}
         ob=json.dumps(dic)
         f.write(ob)
